0f0ec493f0f513e7316cfc593867b912dfe63a86
Angel Ramirez <angel@example.com>
2026-07-19 04:04:10 -0600

    chore: bump version to v1.10.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.labelocr.vskrfe"
     minSdk = 24
     targetSdk = 36
-    versionCode = 20
-    versionName = "1.10.9"
+    versionCode = 21
+    versionName = "1.10.10"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
